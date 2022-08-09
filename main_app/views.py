27555b9df3fc6c6979ae56e9aea7d5737e63fed7
Brian4755 <brian.mccune1@yahoo.com>
2022-08-09 01:08:35 -0500

    error fix

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>This Works</h1>')
+
+
+def about(request):
+  return render(request, 'about.html')
