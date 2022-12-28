8398cfc0657ccb104ba39600b58b84baf7c2f008
Brian4755 <brian.mccune1@yahoo.com>
2022-12-28 07:01:26 -0600

    testing

--- showpro/settings.py
+++ showpro/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True' 
 
-ALLOWED_HOSTS = ["showpro.up.railway.app/", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = ["https://showpro.up.railway.app/"]
 
